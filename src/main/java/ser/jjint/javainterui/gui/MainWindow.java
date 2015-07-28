37c496068630c083567647ba110360c8a12b7152
ghostfox <brueradamian@gmail.com>
2015-07-28 18:06:29 -0300

    Minor Changes in UI

--- src/main/java/ser/jjint/javainterui/gui/MainWindow.java
+++ src/main/java/ser/jjint/javainterui/gui/MainWindow.java
@@ -100,6 +100,8 @@ public class MainWindow extends javax.swing.JFrame {
 
         getContentPane().add(pnlFooter, java.awt.BorderLayout.PAGE_END);
 
+        pnlFunctions.setBorder(javax.swing.BorderFactory.createTitledBorder("Opciones"));
+
         pnlOrderFind.setBorder(javax.swing.BorderFactory.createTitledBorder("Buscar Orden"));
 
         lblCampoBusqueda.setText("Campo Busqueda");
@@ -166,7 +168,7 @@ public class MainWindow extends javax.swing.JFrame {
                         .addComponent(radFindNroCliente)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(radFindNroOrden)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 2, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlOrderFindLayout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnFindOrder)
@@ -177,6 +179,7 @@ public class MainWindow extends javax.swing.JFrame {
         pnlOrderListCriteria.setBorder(javax.swing.BorderFactory.createTitledBorder("Criterios de Ordenamiento"));
 
         btnGroupRadOrder.add(radCriteriaNroOrden);
+        radCriteriaNroOrden.setSelected(true);
         radCriteriaNroOrden.setText("Nro Orden");
 
         btnGroupRadOrder.add(radCriteriaEstadoOrden);
@@ -234,7 +237,7 @@ public class MainWindow extends javax.swing.JFrame {
             .addGroup(pnlHigherFunctionsLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(btnNuevaOrden)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnCambioEstadoOrden)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnAbmItem)
@@ -260,12 +263,14 @@ public class MainWindow extends javax.swing.JFrame {
                 .addComponent(pnlOrderFind, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(pnlOrderListCriteria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(pnlHigherFunctions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(pnlHigherFunctions, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28))
         );
 
         getContentPane().add(pnlFunctions, java.awt.BorderLayout.LINE_START);
+
+        jScrollPane2.setViewportBorder(javax.swing.BorderFactory.createTitledBorder("Ordenes Creadas"));
 
         tabOrdenes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
